abfc2bb158f904315435f1520f95623914e8f4fc
IvoCorrea <ivoaurelio30@gmail.com>
2026-05-18 10:31:24 -0300

    feat: Catalogo, Contato, Galeria, Hero, Navbar e Videos com seus respectivos testes e estilos

---
+++ src/app/components/catalogo/catalogo.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  imports: [],
+  templateUrl: './catalogo.html',
+})
+export class Catalogo {
+
+}
---
+++ src/app/components/galeria/galeria.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria',
+  imports: [],
+  templateUrl: './galeria.html',
+})
+export class Galeria {
+
+}
