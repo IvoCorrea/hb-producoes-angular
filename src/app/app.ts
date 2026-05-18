8471eb52851e1e4bb5908251a7a43aa4ddfe4c75
IvoCorrea <ivoaurelio30@gmail.com>
2026-05-18 10:31:39 -0300

    refactor: simplifica configuração do servidor e remove arquivos desnecessários
    - Remove a importação de rotas do servidor e ajusta a configuração do servidor para usar apenas o fornecimento de renderização.
    - Exclui o arquivo de estilo app.css que não estava sendo utilizado.
    - Limpa o arquivo app.html, removendo o conteúdo de espaço reservado e mantendo apenas os componentes necessários.
    - Atualiza o arquivo app.ts para incluir as importações corretas dos componentes.
    - Ajusta a função de bootstrap no main.server.ts para aceitar o contexto.
    - Adiciona estilos globais ao styles.css, incluindo a importação do Tailwind CSS e define a fonte padrão do corpo.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { Catalogo } from './components/catalogo/catalogo';
+import { Galeria } from './components/galeria/galeria';
+import { Videos } from './components/videos/videos';
+import { Contato } from './components/contato/contato';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Navbar, Hero, Catalogo, Galeria, Videos, Contato],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('hb-producoes');
